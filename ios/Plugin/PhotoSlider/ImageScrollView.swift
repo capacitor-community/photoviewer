45c8e80280d2afe192180b9310d02d7c6adc363d
jepiqueau <jepi.queau@free.fr>
2021-03-06 18:33:36 +0100

    add missing features (zoom, share, base64)

---
+++ ios/Plugin/PhotoSlider/ImageScrollView.swift
@@ -0,0 +1,9 @@
+//
+//  ImageScrollView.swift
+//  Plugin
+//
+//  Created by  Quéau Jean Pierre on 04/03/2021.
+//  Copyright © 2021 Max Lynch. All rights reserved.
+//
+
+import Foundation
---
+++ ios/Plugin/PhotoSlider/ImageScrollViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ImageScrollViewController.swift
+//  Plugin
+//
+//  Created by  Quéau Jean Pierre on 05/03/2021.
+//  Copyright © 2021 Max Lynch. All rights reserved.
+//
+
+import Foundation
