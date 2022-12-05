e8b00cfe5eae0b9c960863d70251690c874b7f04
QUEAU Jean Pierre <jepi.queau@free.fr>
2022-12-05 22:52:23 +0100

    Add swipe-up & swipe-down gesture

---
+++ android/src/main/java/com/getcapacitor/community/media/photoviewer/listeners/OnSwipeTouchListener.kt
@@ -0,0 +1,62 @@
+package com.getcapacitor.community.media.photoviewer.listeners
+
+import android.content.Context
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+
+internal open class OnSwipeTouchListener (c: Context?) :
+  View.OnTouchListener {
+  private val gestureDetector: GestureDetector
+
+  override fun onTouch(view: View?, motionEvent: MotionEvent?): Boolean {
+    return gestureDetector.onTouchEvent(motionEvent)
+  }
+  private inner class GestureListener: GestureDetector.SimpleOnGestureListener() {
+    private val SWIPE_THRESHOLD: Int = 100
+    private val SWIPE_VELOCITY_THRESHOLD: Int = 200
+    override fun onFling(
+      downEvent: MotionEvent?,
+      moveEvent: MotionEvent?,
+      velocityX: Float,
+      velocityY: Float
+    ): Boolean {
+      var diffX = moveEvent?.x?.minus(downEvent!!.x) ?: 0.0F
+      var diffY = moveEvent?.y?.minus(downEvent!!.y) ?: 0.0F
+
+      return if(Math.abs(diffX) > Math.abs(diffY)) {
+        // this is a left or right swipe
+        if(Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+          if(diffX > 0) {
+            onSwipeRight()
+          } else {
+            onSwipeLeft()
+          }
+          true
+        } else {
+          super.onFling(downEvent, moveEvent, velocityX, velocityY)
+       }
+      } else {
+        // this is a up or down swipe
+        if(Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+          if(diffY > 0) {
+            onSwipeDown()
+          } else {
+            onSwipeUp()
+          }
+          true
+        } else {
+          super.onFling(downEvent, moveEvent, velocityX, velocityY)
+        }
+}
+}
+}
+open fun onSwipeRight() {}
+open fun onSwipeLeft() {}
+open fun onSwipeUp() {}
+open fun onSwipeDown() {}
+init {
+gestureDetector = GestureDetector(c, GestureListener())
+}
+
+}
