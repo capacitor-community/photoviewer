4c8569374f9cfdf4abe9772aa5cb40b053b969f6
Eric Horodyski <horodyski@ionic.io>
2024-08-20 11:56:47 -0400

    fix: address #56

--- android/src/main/java/com/getcapacitor/community/media/photoviewer/listeners/OnSwipeTouchListener.kt
+++ android/src/main/java/com/getcapacitor/community/media/photoviewer/listeners/OnSwipeTouchListener.kt
@@ -16,12 +16,12 @@ internal open class OnSwipeTouchListener (c: Context?) :
     private val SWIPE_THRESHOLD: Int = 100
     private val SWIPE_VELOCITY_THRESHOLD: Int = 200
     override fun onFling(
-      downEvent: MotionEvent,
+      downEvent: MotionEvent?,
       moveEvent: MotionEvent,
       velocityX: Float,
       velocityY: Float
     ): Boolean {
-      var diffX = moveEvent.x.minus(downEvent.x)
+      var diffX = moveEvent.x.minus(downEvent!!.x)
       var diffY = moveEvent.y.minus(downEvent.y)
 
       return if(Math.abs(diffX) > Math.abs(diffY)) {
